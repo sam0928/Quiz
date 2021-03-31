c8bff8cb2176c2b12dfc37c52548b3ac82f9222e
sam0928 <sam813189@gmail.com>
2021-04-01 00:29:19 +0800

    quiz2

---
+++ quiz2.js
@@ -0,0 +1,15 @@
+var tags = "李知恩";
+var dataUrl = "https://api.flickr.com/services/feeds/photos_public.gne?tags=" + tags + "&tagmode=any&format=json&per_page=400&jsoncallback=?";
+var data = $.getJSON(dataUrl);
+
+data.done( function( msg ) {
+  console.log(msg.item);
+  $.each(msg.items, function(i, item){
+    $("#contain").append($("<img/>").attr("src", item.media.m));
+  });
+});
+
+data.fail( function( msg ) {
+  console.log(msg);
+  $("#contain").html("fail getting data");
+});
